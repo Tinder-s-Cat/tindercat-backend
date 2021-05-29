46044ab4b4de8834a5a30a637eeb48df3cfa22fa
ekhaer <emiliakhaer@gmail.com>
2021-05-29 22:44:41 +0700

    Authorization Message

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,10 +1,17 @@
 const express = require('express')
-const userRouter = express.Router()
+const messgaeRouter = express.Router()
 const messageController = require('../controller/messageController')
+const authentication = require("../Middlewares/authentication");
+const authMsg = require("../Middlewares/authorizationMessage");
 
-userRouter.get("/matched", messageController.getMatched)
-userRouter.get("/chatroom", messageController.getChatroom)
-userRouter.get("/chatroom/:id", messageController.getMessage)
-userRouter.post("/chatroom/:id", messageController.postMessage)
 
-module.exports = userRouter
+messgaeRouter.use(authentication);
+messgaeRouter.get("/matched", messageController.getMatched)
+messgaeRouter.get("/chatroom", messageController.getChatroom)
+
+// messgaeRouter.get("/chatroom/:id/:isMatchId", messageController.getMessage)
+// messgaeRouter.use(authMsg);
+messgaeRouter.get("/chatroom/:id/:isMatchId", authMsg, messageController.getMessage)
+messgaeRouter.post("/chatroom/:id", messageController.postMessage)
+
+module.exports = messgaeRouter
