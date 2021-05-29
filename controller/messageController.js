69c0b4424431214b7342f6b07ac33dee22b56cd5
ekhaer <emiliakhaer@gmail.com>
2021-05-29 15:04:00 +0700

    Edit table Chatroom; column name from 'IsMatchedId' becomes 'IsMatchId'

--- controller/messageController.js
+++ controller/messageController.js
@@ -1,16 +1,54 @@
 const { Message, IsMatch, ChatRoom, User } = require('../models')
-
+const { v4: uuidv4 } = require('uuid');
+const { Op } = require("sequelize");
 class MessageController {
+    
     //to show the matched users
     static Matched(req, res, next) {
         IsMatch.findAll({
-            include : {
+            include : [{
                 model : User,
-                attributes : ['username']
+                as : 'User',
+                attributes : ['id', 'username', 'location', 'email', 'profilePicture']
+            },
+            {
+                model : User,
+                as : 'Owner',
+                attributes : ['id', 'username', 'location', 'email', 'profilePicture']
+            }
+        ], where : {
+            status : "Match",
+            [Op.or] : [
+                {
+                    UserId : {
+                        [Op.eq] : req.loggedUser.id
+                    }
+                },
+                {
+                    OwnerId : {
+                        [Op.eq] : req.loggedUser.id
+                    }
+                }
+            ]
+        }
+        })
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch((err) => {
+            console.log(err, "error ni")
+            next(err);
+        });
+    }
+
+    //show chatroom based on matched user
+    static Chatroom(req, res, next) {
+        ChatRoom.findAll({
+            include: {
+                model : IsMatch
             }
         })
         .then(data => {
-            console.log(data)
             res.status(200).json(data)
         })
         .catch((err) => {
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -3,5 +3,6 @@ const userRouter = express.Router()
 const messageController = require('../controller/messageController')
 
 userRouter.get("/matched", messageController.Matched)
+userRouter.get("/chatroom", messageController.Chatroom)
 
 module.exports = userRouter
