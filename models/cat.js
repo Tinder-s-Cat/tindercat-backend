7d5f536e391aaee44f54ea55409e12b7e84dbf62
Jacky Rahman <jackyrahman14@gmail.com>
2021-06-01 21:44:19 +0700

    change model assosiate cat to islike with hasMany

--- models/cat.js
+++ models/cat.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Cat.belongsTo(models.User),
-      Cat.hasOne(models.IsLike)
+      Cat.hasMany(models.IsLike)
     }
   };
   Cat.init({
